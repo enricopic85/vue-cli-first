6eec6112dbf7d0938e19a9c9ad9c594a02819d18
enricopic85 <enricodev85@gmail.com>
2022-01-26 15:31:57 +0100

    primo esercizio

---
+++ src/components/Hero.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="jumbo">
+    <img src="https://miro.medium.com/max/855/1*9A5EFag9rsRWjWswxlZ4hA.png" alt="">
+</div>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
+
+<style>
+*{
+    margin:0,
+    padding:0,
+    box-sizing: border-box
+}
+.jumbo{
+    width: 100%;
+    height: 500px;
+}
+.jumbo img{
+    width: 100%;
+    margin: 0 auto;
+}
+
+</style>
